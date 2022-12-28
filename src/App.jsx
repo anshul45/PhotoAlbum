dbea98c8662bb66b093af0c3e8e56c959e3cb304
anshul45 <80778090+anshul45@users.noreply.github.com>
2022-12-28 11:50:16 +0530

    matrial-ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,31 @@
 import React from "react";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedium,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from "@mui/material";
+
+import CameraIcon from "@mui/icons-material/PhotoCamera";
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon />
+          <Typography></Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default App;
