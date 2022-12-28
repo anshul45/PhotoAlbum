103aed90ddaf2cace7ebf3df0ae70bdb20cd5b82
anshul45 <80778090+anshul45@users.noreply.github.com>
2022-12-28 12:39:11 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,33 @@ const App = () => {
       <AppBar position="relative">
         <Toolbar>
           <CameraIcon />
-          <Typography></Typography>
+          <Typography variant="h6"> Photo Album</Typography>
         </Toolbar>
       </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello everyone This is a photo album and i'm trying to make this
+              sentences as long as possible so we can see how does it look like
+              on the screen
+            </Typography>
+          </Container>
+        </div>
+      </main>
     </>
   );
 };
